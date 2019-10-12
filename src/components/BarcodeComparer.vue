2c2814aff0b38db81f54bcb164b87e84e9408f0a
Joe S <joe@skrd.uk>
2019-10-12 23:43:37 +0100

    Create initial working app

---
+++ src/components/BarcodeComparer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p class="text-center mb-5">
+      <button class="btn btn-secondary btn-lg" v-on:click="clear()">Clear</button>
+    </p>
+    <div class="row">
+      <div class="col-6">
+        <Card title="Covers" :barcodes="covers" :comparedBarcodes="inners"></Card>
+      </div>
+      <div class="col-6">
+        <Card title="Inners" :barcodes="inners" :comparedBarcodes="covers"></Card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './BarcodeComparerCard.vue'
+
+export default {
+  name: 'BarcodeComparer',
+  components: {
+    Card
+  },
+  data: function () {
+    return {
+      covers: [],
+      inners: []
+    }
+  },
+  methods: {
+    clear: function () {
+      this.covers = []
+      this.inners = []
+    }
+  }
+}
+</script>
---
+++ src/components/BarcodeComparerCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title text-center">
+        <a href="#" class="text-dark stretched-link" v-on:click="$refs.barcode.focus()">{{ title }}</a>
+      </h4>
+      <table class="table table-sm text-monospace small">
+        <tbody>
+          <tr v-for="(barcode, index) in barcodes" :key="index" :class="context(index)">
+            <th scope="row">{{ index + 1 }}</th>
+            <td>{{ barcode }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <form v-on:submit.prevent="append">
+        <input :id="title" type="text" class="form-control" placeholder="Scan barcode" ref="barcode">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BarcodeComparerCard',
+  props: {
+    title: String,
+    barcodes: Array,
+    comparedBarcodes: Array
+  },
+  methods: {
+    context: function (index) {
+      return {
+        'table-success': this.matches(index) === true,
+        'table-danger': this.matches(index) === false
+      }
+    },
+    matches: function (index) {
+      if (this.barcodes[index] === this.comparedBarcodes[index])
+        return true
+
+      if (this.comparedBarcodes.includes(this.barcodes[index]))
+        return false
+
+      return null
+    },
+    append: function () {
+      this.barcodes.push(this.$refs.barcode.value)
+      this.$refs.barcode.value = ''
+    }
+  }
+}
+</script>
